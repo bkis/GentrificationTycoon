8e4bc7beb53cfd7cca1509dd3b5632e917bdae5b
kritzelbit <boerge.kiss@gmail.com>
2014-01-16 10:46:26 +0100

    fixed imports

--- src/gtpreview/InputMapper.java
+++ src/gtpreview/InputMapper.java
@@ -15,7 +15,6 @@ import com.jme3.math.Ray;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
 
 /**
